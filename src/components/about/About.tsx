9fbf40b45a81b966229c9eda7eda5234f59bad88
isabellaes <borjessonisabella@gmail.com>
2024-03-25 12:48:01 +0100

    update about - add img

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,35 +1,43 @@
 import "./about.scss";
+import profilbild from "../../assets/profilbild.jpg";
 
 const About = () => {
   return (
     <div className="about">
       <h1>Om mig</h1>
+
       <p>
+        <img src={profilbild} alt="profil-bild" />
         Hej! Jag heter Isabella och är en passionerad och ambitiös
         utvecklarstudent med starka problemlösningsförmågor och analytiskt
         tänkande. Min programmeringsresa började för ungefär tre år sedan när
         jag påbörjade min utbildning som systemutvecklare och fördjupade mig i
-        grunderna av C#. Det dröjde inte länge innan jag utvecklade en djup
-        passion för kodning. Under de följande två åren ägnade jag mig
+        grunderna av C# och .Net. Det dröjde inte länge innan jag utvecklade en
+        djup passion för kodning. Under de följande två åren ägnade jag mig
         helhjärtat åt att lära mig och utveckla mina färdigheter. Medan mitt
         fokus främst låg på backend-utveckling, fördjupade jag mig även i
-        frontend-utveckling under min utbildning. I juni 2023 tog jag stolt emot
-        min examen som systemutvecklare. Trots att jag aktivt sökte
-        jobbmöjligheter, stötte jag på utmaningar i den konkurrensutsatta
-        marknaden, med få företag som var i en position att anställa. Som ett
-        resultat av detta beslutade jag mig för att fortsätta min utbildning vid
-        samma institution, denna gång specialiserad på frontend-utveckling. För
-        närvarande är jag i mitt första år av denna utbildning, och jag har
-        redan skaffat mig betydande kunskap och färdigheter. Jag ser med stor
-        förväntan fram emot vad som väntar mig när jag närmar mig examen
-        sommaren 2025. Jag söker för närvarande en praktikplats på ett företag
-        där jag kan tillämpa och utveckla mina färdigheter. Min första
-        praktikplats är planerad att starta i september 2024 och pågå i fyra
-        veckor, följt av en andra praktikplats som startar i januari 2025 och
-        varar ungefär 20 veckor. Jag ser fram emot att hitta en plats där jag
-        kan tillämpa det jag lärt mig och fortsätta att växa. Även om min
-        passion ligger inom webbutveckling är jag öppen för olika möjligheter.
-        Om du är intresserad, tveka inte att kontakta mig!
+        frontend-utveckling under min utbildning.
+        <br />
+        <br />
+        I juni 2023 tog jag stolt emot min examen som systemutvecklare. Trots
+        att jag aktivt sökte jobbmöjligheter, stötte jag på utmaningar i den
+        konkurrensutsatta marknaden, med få företag som var i en position att
+        anställa. Som ett resultat av detta beslutade jag mig för att fortsätta
+        min utbildning vid samma institution, denna gång specialiserad på
+        frontend-utveckling. För närvarande är jag i mitt första år av denna
+        utbildning, och jag har redan skaffat mig betydande kunskap och
+        färdigheter. Jag ser med stor förväntan fram emot vad som väntar mig när
+        jag närmar mig examen sommaren 2025.
+        <br />
+        <br />
+        Jag söker för närvarande en praktikplats på ett företag där jag kan
+        tillämpa och utveckla mina färdigheter. Min första praktikplats är
+        planerad att starta i september 2024 och pågå i fyra veckor, följt av en
+        andra praktikplats som startar i januari 2025 och varar ungefär 20
+        veckor. Jag ser fram emot att hitta en plats där jag kan tillämpa det
+        jag lärt mig och fortsätta att växa. Även om min passion ligger inom
+        webbutveckling är jag öppen för olika möjligheter. Om du är intresserad,
+        tveka inte att kontakta mig!
       </p>
     </div>
   );
